aef67b5e09bee7d992ae612b4a91cc89e8a19c45
Milad Coder <miladcoder1390@gmail.com>
2025-07-29 22:06:48 +0330

    Make menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Menu from "@/components/Menu";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,12 +10,14 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">l</div>
+      <div className="w-[86%]  md:w-[92%] lg:w-[84%] xl:w-[86%] ">l</div>
       {/* RIGHT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        <Link href='/' className="flex items-center justify-center gap-2">
-        <img src="logo.png" alt="" />
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link href='/' className="flex items-center justify-center lg:justify-end gap-2">
+        <span className="hidden lg:block">SMSS</span>
+        <Image src="/logo.png" width={52} height={52} alt="" />
         </Link>
+        <Menu />
       </div>
     </div>
   );
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,88 +1,91 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
     items: [
       {
         icon: "/home.png",
-        label: "Home",
+        label: "خانه",
         href: "/",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/teacher.png",
-        label: "Teachers",
+        label: "معلّم ها",
         href: "/list/teachers",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/student.png",
-        label: "Students",
+        label: "دانش اموزان",
         href: "/list/students",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/parent.png",
-        label: "Parents",
+        label: "والدین",
         href: "/list/parents",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/subject.png",
-        label: "Subjects",
+        label: "موضوعات",
         href: "/list/subjects",
         visible: ["admin"],
       },
       {
         icon: "/class.png",
-        label: "Classes",
+        label: "کلاس ها",
         href: "/list/classes",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/lesson.png",
-        label: "Lessons",
+        label: "دروس",
         href: "/list/lessons",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/exam.png",
-        label: "Exams",
+        label: "امتحانات",
         href: "/list/exams",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/assignment.png",
-        label: "Assignments",
+        label: "تکالیف",
         href: "/list/assignments",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/result.png",
-        label: "Results",
+        label: "نتایج",
         href: "/list/results",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/attendance.png",
-        label: "Attendance",
+        label: "حضور و غیاب",
         href: "/list/attendance",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/calendar.png",
-        label: "Events",
+        label: "رویداد ها",
         href: "/list/events",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/message.png",
-        label: "Messages",
+        label: "پیام ها",
         href: "/list/messages",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/announcement.png",
-        label: "Announcements",
+        label: "اطلاعیه ها",
         href: "/list/announcements",
         visible: ["admin", "teacher", "student", "parent"],
       },
@@ -93,22 +96,41 @@ const menuItems = [
     items: [
       {
         icon: "/profile.png",
-        label: "Profile",
+        label: "پروفایل",
         href: "/profile",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/setting.png",
-        label: "Settings",
+        label: "تنظیمات",
         href: "/settings",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
         icon: "/logout.png",
-        label: "Logout",
+        label: "خروج از حساب",
         href: "/logout",
         visible: ["admin", "teacher", "student", "parent"],
       },
     ],
   },
 ];
+const Menu = () =>{ 
+  return(
+    <div className="flex flex-col items-end"> 
+    {menuItems.map(i => (
+      <div key={i.title} className="flex flex-col gap-2">
+        <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+        {i.items.map(item => 
+          <Link href={item.href} key={item.href} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2" >
+            <Image src={item.icon} width={20} height={20} alt={item.label}/>
+            <span className="hidden lg:block">{item.label}</span>
+          </Link>
+        )}
+      </div>
+    ))}
+   </div>
+  )
+}
+
+export default Menu;
