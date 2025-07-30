19ee12f894e2b2e9aa0662d4f0e694b8b1e0a45e
Milad Coder <miladcoder1390@gmail.com>
2025-07-30 21:51:58 +0330

    Making finance table

---
+++ src/components/FinanceChart.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import Image from "next/image"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: 'مهر',
+        درامد: 400000000,
+        هزینه: 2400000,
+
+    },
+    {
+        name: 'ابان',
+        درامد: 3000000000,
+        هزینه: 130098,
+
+    },
+    {
+        name: 'اذر',
+        درامد: 20000000,
+        هزینه: 199800,
+
+    },
+    {
+        name: 'دی',
+        درامد: 27000000080,
+        هزینه: 3900008,
+
+    },
+    {
+        name: 'بهمن',
+        درامد: 18000090,
+        هزینه: 48000000,
+
+    },
+    {
+        name: 'اسفند',
+        درامد: 2300090,
+        هزینه: 38000,
+
+    },
+    {
+        name: 'فروردین',
+        درامد: 3400090,
+        هزینه: 4300000,
+
+    },
+
+    {
+        name: 'اردیبهشت',
+        درامد: 3400090,
+        هزینه: 43000,
+
+    },
+    {
+        name: 'خرداد',
+        درامد: 34000090,
+        هزینه: 43000,
+
+    },
+];
+
+function FinanceChart() {
+    return (
+        <div className='bg-white rounded-xl w-full h-full p-4'>
+            <div className='flex justify-between items-center'>
+                <h1 className='text-lg font-semibold'>مالی ( تومان)</h1>
+                <Image src='/moreDark.png' width={20} height={20} alt='' />
+            </div>
+            <ResponsiveContainer width="100%" height="90%">
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" stroke="#ddd" />
+                    <XAxis dataKey="name" axisLine={false} tick={{ fill: '#d1d5db' }} tickLine={false} tickMargin={10} />
+                    <YAxis axisLine={false} tick={{ fill: '#d1d5db' }} tickLine={false} tickMargin={10} />
+                    <Tooltip />
+                    <Legend align='center' verticalAlign='top' wrapperStyle={{paddingTop: '10px', paddingBottom: '30px'}}/>
+                    <Line type="monotone" dataKey="درامد" stroke="#C3EBFA" strokeWidth={5} />
+                    <Line type="monotone" dataKey="هزینه" stroke="#CFCEFF" strokeWidth={5} />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+
+export default FinanceChart
