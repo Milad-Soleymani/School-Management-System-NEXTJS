cfbf21cdf219ed88f33863c237bbe46926f04401
Milad Coder <miladcoder1390@gmail.com>
2025-07-30 21:04:46 +0330

    Make Attendance chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
@@ -6,9 +7,9 @@ function AdminPage() {
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-6">
-      {/* USER CARDS */}
+        {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard  type="دانش اموزان" />
+          <UserCard type="دانش اموزان" />
           <UserCard type="معلم ها" />
           <UserCard type="والدین" />
           <UserCard type="پرسنل" />
@@ -17,12 +18,14 @@ function AdminPage() {
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART  */}
           <div className="w-full lg:w-1/3 h-[450px]">
-          <CountChart />
+            <CountChart />
           </div>
           {/* ATTENDANCE CHART  */}
-          <div className="w-full lg:w-2/3 h-[450px] "></div>
+          <div className="w-full lg:w-2/3 h-[450px] ">
+          <AttendanceChart />
+          </div>
         </div>
-        
+
         {/* BOTTOM CHART */}
         <div className=""></div>
       </div>
