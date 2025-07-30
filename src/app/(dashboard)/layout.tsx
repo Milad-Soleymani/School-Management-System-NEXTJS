4b014c34fe831f4bfb259ce53ff1368d5ddc4d32
Milad Coder <miladcoder1390@gmail.com>
2025-07-30 21:19:14 +0330

    Debug

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[86%]  md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] ">
+      <div className="w-[86%]  md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]  overflow-scroll ">
         <NavBar />
         {children}
       </div>
