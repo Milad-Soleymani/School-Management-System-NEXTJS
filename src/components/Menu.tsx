5c2ca42bc85175af2796d5d0b118e0c78e4da436
Milad Coder <miladcoder1390@gmail.com>
2025-07-31 18:07:20 +0330

    Debug

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -117,14 +118,14 @@ const menuItems = [
 ];
 const Menu = () => {
   return (
-    <div className="flex flex-col items-end font-thin text-[13px]">
+    <div className="flex flex-col items-end font-thin w-full text-[13px]">
       {menuItems.map(i => (
-        <div key={i.title} className="flex flex-col p-0 ">
+        <div key={i.title} className="flex flex-col p-0 w-full items-center">
           <span className="hidden lg:block text-gray-400 font-semibold my-4">{i.title}</span>
           {i.items.map(item => {
-            if (item.visible.includes('admin')) {
+            if (item.visible.includes(role)) {
               return (
-                <Link href={item.href} key={item.href} className="flex  lg:justify-end gap-4 text-gray-500 py-1.5 hover:text-gray-950" >
+                <Link href={item.href} key={item.href} className="flex items-center justify-center w-[100%]  lg:justify-end gap-4 text-gray-500 py-1.5 rounded-md  hover:bg-blueSkyLight" >
                   <span className="hidden lg:block font-medium">{item.label}</span>
                   <Image src={item.icon} width={18} height={18} alt={item.label} />
                 </Link>
