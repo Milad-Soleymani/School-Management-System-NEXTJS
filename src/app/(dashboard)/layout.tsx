b08f7bd5220ba25444faeb3708db9e23764bf6fb
Milad Coder <miladcoder1390@gmail.com>
2025-07-31 17:28:58 +0330

    Redesign

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
       {/* RIGHT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href='/' className="flex items-center justify-center lg:justify-end gap-2">
-        <span className="hidden lg:block">سامانه مدیریت مدارس</span>
+        <span className="hidden lg:block font-bold">سامانه مدیریت مدارس</span>
         <Image src="/logo.png" width={52} height={52} alt="" />
         </Link>
         <Menu />
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -120,10 +120,10 @@ const Menu = () =>{
     <div className="flex flex-col items-end font-thin text-[13px]"> 
     {menuItems.map(i => (
       <div key={i.title} className="flex flex-col p-0 ">
-        <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+        <span className="hidden lg:block text-gray-400 font-bold my-4">{i.title}</span>
         {i.items.map(item => 
           <Link href={item.href} key={item.href} className="flex  lg:justify-end gap-4 text-gray-500 py-1.5 hover:text-gray-950" >
-            <span className="hidden lg:block">{item.label}</span>
+            <span className="hidden lg:block font-medium">{item.label}</span>
             <Image src={item.icon} width={18} height={18} alt={item.label}/>
           </Link>
         )}
