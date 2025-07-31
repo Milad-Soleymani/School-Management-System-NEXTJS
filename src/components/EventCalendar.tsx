bbb58f109fd290a3a693a52ec94e6b97f8597e08
Milad Coder <miladcoder1390@gmail.com>
2025-07-31 14:32:35 +0330

    Debug

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -8,19 +8,19 @@ import "react-modern-calendar-datepicker/lib/DatePicker.css";
 const events = [
   {
     id: 1,
-    title: "Lorem ipsum dolor",
-    time: "12:00 PM - 2:00 PM",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "لورم ایپسوم",
+    time: "۱۲:۰۰ PM - ۲:۰۰ PM",
+    description: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.",
   }, {
     id: 2,
-    title: "Lorem ipsum dolor",
-    time: "12:00 PM - 2:00 PM",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "لورم ایپسوم",
+    time: "۱۲:۰۰ PM - ۲:۰۰ PM",
+    description: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.",
   }, {
     id: 3,
-    title: "Lorem ipsum dolor",
-    time: "12:00 PM - 2:00 PM",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    title: "لورم ایپسوم",
+    time: "۱۲:۰۰ PM - ۲:۰۰ PM",
+    description: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.",
   }
 ]
 function EventCalendar() {
@@ -43,10 +43,10 @@ function EventCalendar() {
         {events.map(event => (
           <div className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-blueSky even:border-t-specialPurple" key={event.id}>
             <div className="flex items-center justify-between">
-              <h1 className='font-semibold text-gray-600'>{event.title}</h1>
               <span className='text-gray-300 text-sm'>{event.time}</span>
+              <h1 className='font-semibold text-gray-600'>{event.title}</h1>
             </div>
-            <p className='mt-2 text-gray-400 text-sm'>{event.description}</p>
+            <p className='mt-2 text-gray-400 text-sm text-right'>{event.description}</p>
           </div>
         ))}
       </div>
