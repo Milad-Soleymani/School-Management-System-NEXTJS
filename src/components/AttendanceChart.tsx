a811d626a11d68f3debda98e705ab5c0cac6ff82
Milad Coder <miladcoder1390@gmail.com>
2025-07-31 11:37:55 +0330

    Debug

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -40,7 +40,7 @@ function AttendanceChart() {
     return (
         <div className='bg-white rounded-lg p-4 h-full'>
             <div className="flex justify-between items-center">
-                <h1 className='text-lg font-semibold'>Attendance</h1>
+                <h1 className='text-lg font-semibold'>حضور و غایب</h1>
                 <Image src='/moreDark.png' alt='' width={20} height={20} />
             </div>
             <ResponsiveContainer width="100%" height="90%">
