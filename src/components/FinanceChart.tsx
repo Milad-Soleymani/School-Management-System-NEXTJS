54bcf698195029828d9de3c9d5452697fb46321f
Milad Coder <miladcoder1390@gmail.com>
2025-07-31 14:43:06 +0330

    Debug

--- src/components/FinanceChart.tsx
+++ src/components/FinanceChart.tsx
@@ -4,28 +4,29 @@ import Image from "next/image"
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
+
     {
-        name: 'مهر',
-        درامد: 400,
-        هزینه: 240,
+        name: 'خرداد',
+        درامد: 30,
+        هزینه: 40,
 
     },
     {
-        name: 'ابان',
-        درامد: 3000,
-        هزینه: 98,
+        name: 'اردیبهشت',
+        درامد: 90,
+        هزینه: 4,
 
     },
     {
-        name: 'اذر',
-        درامد: 200,
-        هزینه: 199,
+        name: 'فروردین',
+        درامد: 390,
+        هزینه: 50,
 
     },
     {
-        name: 'دی',
-        درامد: 2700,
-        هزینه: 3900,
+        name: 'اسفند',
+        درامد: 20,
+        هزینه: 90,
 
     },
     {
@@ -35,32 +36,30 @@ const data = [
 
     },
     {
-        name: 'اسفند',
-        درامد: 20,
-        هزینه:90,
+        name: 'دی',
+        درامد: 2700,
+        هزینه: 3900,
 
     },
     {
-        name: 'فروردین',
-        درامد: 390,
-        هزینه: 50,
-
-    },
-
-    {
-        name: 'اردیبهشت',
-        درامد: 90,
-        هزینه: 4,
+        name: 'اذر',
+        درامد: 200,
+        هزینه: 199,
 
     },
     {
-        name: 'خرداد',
-        درامد: 30,
-        هزینه: 40,
+        name: 'ابان',
+        درامد: 3000,
+        هزینه: 98,
+
+    },
+    {
+        name: 'مهر',
+        درامد: 400,
+        هزینه: 240,
 
     },
 ];
-
 function FinanceChart() {
     return (
         <div className='bg-white rounded-xl w-full h-full p-4'>
@@ -84,7 +83,7 @@ function FinanceChart() {
                     <XAxis dataKey="name" axisLine={false} tick={{ fill: '#d1d5db' }} tickLine={false} tickMargin={10} />
                     <YAxis axisLine={false} tick={{ fill: '#d1d5db' }} tickLine={false} tickMargin={10} />
                     <Tooltip />
-                    <Legend align='center' verticalAlign='top' wrapperStyle={{paddingTop: '10px', paddingBottom: '30px'}}/>
+                    <Legend align='center' verticalAlign='top' wrapperStyle={{ paddingTop: '10px', paddingBottom: '30px' }} />
                     <Line type="monotone" dataKey="درامد" stroke="#C3EBFA" strokeWidth={5} />
                     <Line type="monotone" dataKey="هزینه" stroke="#CFCEFF" strokeWidth={5} />
                 </LineChart>
