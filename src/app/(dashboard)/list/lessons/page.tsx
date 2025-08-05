70492c86793133e5615054da1368c3c78b0c5760
Milad Coder <miladcoder1390@gmail.com>
2025-08-05 13:32:49 +0330

    Making lesson list page

---
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -0,0 +1,96 @@
+import Pagination from '@/components/Pagination'
+import Table from '@/components/Table'
+import TableSearch from '@/components/TableSearch'
+import { classesData, lessonsData, role } from '@/lib/data'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+type Lesson = {
+    id: number;
+    subject: string;
+    class: string;
+    teacher: string;
+}
+
+const columns = [
+    {
+        header: "نام کلاس",
+        accessor: "name"
+    }, {
+        header: "کلاس",
+        accessor: "class",
+        className: "hidden md:table-cell"
+
+    }, {
+        header: "معلم",
+        accessor: "teacher",
+        className: "hidden md:table-cell"
+    }, 
+    {
+        header: "اعمال",
+        accessor: "actions",
+    }
+
+]
+
+const LessonListPage = () => {
+
+    const renderRow = (item: Lesson) => (
+        <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-specialPurpleLight'>
+            <td className='flex items-center gap-4 p-4'>
+                {item.subject}
+
+            </td>
+            <td>{item.class}</td>
+            <td className='hidden md:table-cell'>{item.teacher}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                    <Link href={`/list/teachers/${item.id}`}>
+                        <button className='w-7 h-7 flex items-center justify-center rounded-full bg-blueSky'>
+                            <Image src='/edit.png' alt='' width={16} height={16} />
+                        </button>
+                    </Link>
+                    {role === "admin" && (
+                        <button className='w-7 h-7 flex items-center justify-center rounded-full bg-specialPurple'>
+                            <Image src='/delete.png' alt='' width={16} height={16} />
+                        </button>)}
+                </div>
+            </td>
+        </tr>
+    );
+
+    return (
+        <div className='bg-white p-4 rounded-md flex-1  m-4 mt-0'>
+            {/* TOP */}
+            <div className="flex justify-between">
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+
+                    <div className="flex items-center gap-4 self-end">
+                        <button className='w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow'>
+                            <Image src='/filter.png' width={14} height={14} alt='' />
+                        </button>
+                        <button className='w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow'>
+                            <Image src='/sort.png' width={14} height={14} alt='' />
+                        </button>
+                        {role === 'admin' && <button className='w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow'>
+                            <Image src='/plus.png' width={14} height={14} alt='' />
+                        </button>}
+                        <TableSearch />
+                    </div>
+                </div>
+                <h1 className='hidden  md:block text-lg font-semibold '> همه دروس </h1>
+
+            </div>
+
+            {/* LIST */}
+            <Table columns={columns} renderRow={renderRow} data={lessonsData} />
+            {/* PAGINATION */}
+            <Pagination />
+
+
+        </div>
+    )
+}
+
+export default LessonListPage
