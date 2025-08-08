2742888835f59e5b6a74294f11022b33a153209a
Milad Coder <miladcoder1390@gmail.com>
2025-08-08 13:37:53 +0330

    Using form modal in lesson list page

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from '@/components/FormModal'
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
@@ -26,7 +27,7 @@ const columns = [
         header: "معلم",
         accessor: "teacher",
         className: "hidden md:table-cell"
-    }, 
+    },
     {
         header: "اعمال",
         accessor: "actions",
@@ -46,15 +47,13 @@ const LessonListPage = () => {
             <td className='hidden md:table-cell'>{item.teacher}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
-                        <button className='w-7 h-7 flex items-center justify-center rounded-full bg-blueSky'>
-                            <Image src='/edit.png' alt='' width={16} height={16} />
-                        </button>
-                    </Link>
                     {role === "admin" && (
-                        <button className='w-7 h-7 flex items-center justify-center rounded-full bg-specialPurple'>
-                            <Image src='/delete.png' alt='' width={16} height={16} />
-                        </button>)}
+                        <>
+                            <FormModal table='subject' type='update' data={item} />
+                            <FormModal table='subject' type='delete' id={item.id} />
+
+                        </>
+                    )}
                 </div>
             </td>
         </tr>
@@ -73,9 +72,9 @@ const LessonListPage = () => {
                         <button className='w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow'>
                             <Image src='/sort.png' width={14} height={14} alt='' />
                         </button>
-                        {role === 'admin' && <button className='w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow'>
-                            <Image src='/plus.png' width={14} height={14} alt='' />
-                        </button>}
+                        {role === 'admin' &&
+                            <FormModal table='subject' type='create' />
+                        }
                         <TableSearch />
                     </div>
                 </div>
