d48382098a736e57607ec4f8e525abaf688fa38c
Milad Coder <miladcoder1390@gmail.com>
2025-08-08 14:19:29 +0330

    Making attractive

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Image from "next/image";
+import { useState } from "react";
 
 function FormModal({ table, type, data, id }: {
     table:
@@ -16,22 +17,39 @@ function FormModal({ table, type, data, id }: {
     | "attendance"
     | "event"
     | "announcement";
-    type:"create" | "update" | "delete";
-    data?:any;
-    id?:number;
+    type: "create" | "update" | "delete";
+    data?: any;
+    id?: number;
 }
 ) {
     const size = type === "create" ? "w-8 h-8 " : "w-7 h-7"
-    const bgColor = type === "create" ? "bg-specialYellow" : type === "update" ? "bg-blueSky" :"bg-specialPurple"
+    const bgColor = type === "create" ? "bg-specialYellow" : type === "update" ? "bg-blueSky" : "bg-specialPurple"
+    const [open, setOpen] = useState(false)
+
+
+    const Form = () => {
+        return type === "delete" && id ? <form action='' className="p-4 flex flex-col gap-4">
+            <span className="text-center font-medium"> All data will be lost. Are you sure you want to delete this {table}? </span>
+            <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
+        </form> : "create or update form"
+    }
     return (
         <>
-        <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`} >
-            <Image src={`/${type}.png`} width={16} height={16} alt="" />
-        </button>
+            <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`} onClick={() => setOpen(true)}>
+                <Image src={`/${type}.png`} width={16} height={16} alt="" />
+            </button>
+            {open && <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex justify-center items-center">
+                <div className="bg-white rounded-md p-4 relative w-[90%] md:w-[70%} lg:w-[65%] xl:w-[50%] 2x;:w-[40%] ">
+                    <Form />
+                    <div className="absolute top-4 right-4 cursor-pointer" onClick={() => setOpen(false)}>
+                        <Image src='/close.png' width={14} height={14} alt="" />
+                    </div>
+                </div>
+            </div>}
         </>
 
 
 
     )
 }
-    export default FormModal
+export default FormModal
