c92d2ddc1f8eb34d634b6e844252b4fac1f39b08
Milad Coder <miladcoder1390@gmail.com>
2025-08-08 16:36:27 +0330

    Adding vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "School Management System",
