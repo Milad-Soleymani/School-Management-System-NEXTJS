22f08511c0391bfba9672b91660785a506ee8f2b
Milad Coder <miladcoder1390@gmail.com>
2025-08-08 13:48:26 +0330

    Using form modal in announcement list page

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from '@/components/FormModal'
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
@@ -17,16 +18,16 @@ const columns = [
     {
         header: "موضوع",
         accessor: "title"
-    },  {
+    }, {
         header: "کلاس",
         accessor: "class",
         className: "hidden md:table-cell"
 
-    },{
+    }, {
         header: "تاریخ",
         accessor: "date",
         className: "hidden md:table-cell"
-    },{
+    }, {
         header: "اعمال",
         accessor: "actions",
     }
@@ -44,15 +45,13 @@ const AnnouncementListPage = () => {
             <td className='hidden md:table-cell'>{item.date}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
-                        <button className='w-7 h-7 flex items-center justify-center rounded-full bg-blueSky'>
-                            <Image src='/edit.png' alt='' width={16} height={16} />
-                        </button>
-                    </Link>
                     {role === "admin" && (
-                        <button className='w-7 h-7 flex items-center justify-center rounded-full bg-specialPurple'>
-                            <Image src='/delete.png' alt='' width={16} height={16} />
-                        </button>)}
+                        <>
+                            <FormModal table='subject' type='update' data={item} />
+                            <FormModal table='subject' type='delete' id={item.id} />
+
+                        </>
+                    )}
                 </div>
             </td>
         </tr>
@@ -71,9 +70,9 @@ const AnnouncementListPage = () => {
                         <button className='w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow'>
                             <Image src='/sort.png' width={14} height={14} alt='' />
                         </button>
-                        {role === 'admin' && <button className='w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow'>
-                            <Image src='/plus.png' width={14} height={14} alt='' />
-                        </button>}
+                        {role === 'admin' &&
+                            <FormModal table='subject' type='create' />
+                        }
                         <TableSearch />
                     </div>
                 </div>
