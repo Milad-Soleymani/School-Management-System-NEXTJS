3c0bcdf967f87650ae13576847bce62da4653113
Milad Coder <miladcoder1390@gmail.com>
2025-08-17 16:33:51 +0330

    Adding logo

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -17,9 +17,9 @@ export default function DashboardLayout({
       </div>
       {/* RIGHT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
-        <Link href='/' className="flex items-center justify-center lg:justify-end gap-2">
+        <Link href='/' className="flex items-center justify-center lg:justify-end gap-2 text-right">
         <span className="hidden lg:block font-bold">سامانه مدیریت مدارس</span>
-        <Image src="/logo.png" width={52} height={52} alt="" />
+        <Image src="/logo.png" width={72} height={72} alt="" />
         </Link>
         <Menu />
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from "next/head";
 
 
 export const metadata: Metadata = {
-  title: "School Management System",
-  description: "Next.js School Management System",
+  title: "سامانه مدیریت مدرسه",
+  icons: {
+    icon: '/logo.png'
+  }
 };
 
 export default function RootLayout({
@@ -17,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/logo.png" />
+      </Head>
       <body>{children}</body>
       <Analytics />
       <SpeedInsights />
