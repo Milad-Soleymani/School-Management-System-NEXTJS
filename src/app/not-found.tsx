504e922305fb4b9fc91db77c28ca68ff31473b85
Milad Coder <miladcoder1390@gmail.com>
2025-08-17 15:09:42 +0330

    Redesign

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,11 +13,11 @@ function notfound() {
           <Image src={"/404.png"} width={200} height={200} alt="صفحه پیدا نشد" />
 
 </div>
-          <h1 className="text-4xl text-white font-bold mt-4 mb-2">صفحه مورد نظر یافت نشد</h1>
+          <h1 className="text-4xl font-bold mt-4 mb-2 text-gray-500">صفحه مورد نظر یافت نشد</h1>
           <p className="text-xl text-[#C3EBFA] mb-8">سیستم مدیریت مدرسه</p>
 
           <Link href="/">
-            <button className="py-3 px-6 text-base font-semibold text-white bg-[#FAE27C] rounded-lg cursor-pointer transition-colors duration-300 hover:bg-[#FEFCE8]">
+            <button className="py-3 px-6 text-base font-semibold text-white  bg-[#FAE27C] rounded-lg cursor-pointer transition-colors duration-300 hover:bg-[#E6C24A]">
               بازگشت به صفحه اصلی
             </button>
           </Link>
