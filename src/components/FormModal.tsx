7f419321cfa069b444433aab69f56b083ba9d4be
Milad-Soleymani <miladcoder1390@gmail.com>
2025-09-05 16:35:43 +0330

    Debug

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -34,7 +34,7 @@ function FormModal({
     table: "teacher" | "student" | "parent" | "subject" | "class" | "lesson" | "exam" | "assignment" | "result" | "attendance" | "event" | "announcement";
     type: "create" | "update" | "delete";
     data?: unknown;
-    id?: number;
+    id?: string | number;
 }) {
     const [open, setOpen] = useState(false);
 
