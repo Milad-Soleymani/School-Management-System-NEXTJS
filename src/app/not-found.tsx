55f74e1bf096ce923fc10afdc1e8c63da1d70d46
Milad-Soleymani <miladcoder1390@gmail.com>
2025-09-12 21:12:24 +0330

    Redesign

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,8 +8,8 @@ import React from "react";
  */
 function NotFound() {
   return (
-    <div className="flex justify-center items-center min-h-screen text-center bg-[#EDF9FD]">
-      <main className="p-8 bg-[#F1F0FF] rounded-2xl shadow-lg max-w-xl mx-4">
+    <div className="flex justify-center items-center min-h-screen text-center bg-gradient-to-br from-yellow-400 to-sky-400 text-[#333333]">
+      <main className="p-8 max-w-xl mx-4">
         {/* تصویر یا انیمیشن */}
         <div className="flex justify-center items-center">
           <Image
@@ -20,13 +20,13 @@ function NotFound() {
           />
         </div>
 
-        <h1 className="text-4xl font-bold mt-4 mb-2 text-gray-500">
+        <h1 className="text-4xl font-bold mt-4 mb-2 text-[#4e4e4e]">
           صفحه مورد نظر یافت نشد
         </h1>
-        <p className="text-xl text-[#C3EBFA] mb-8">سیستم مدیریت مدرسه</p>
+        <p className="text-xl text-[#fff] mb-8">سیستم مدیریت مدرسه</p>
 
         <Link href="/">
-          <button className="py-3 px-6 text-base font-semibold text-white bg-[#FAE27C] rounded-lg cursor-pointer transition-colors duration-300 hover:bg-[#E6C24A]">
+          <button className="py-3 px-6 text-base font-semibold text-white bg-[#7cb7ff] rounded-lg cursor-pointer transition-colors duration-300 hover:bg-[#338fff]">
             بازگشت به صفحه اصلی
           </button>
         </Link>
