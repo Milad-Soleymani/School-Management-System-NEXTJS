cb8b8f54f369df5239723515190cd1b2286770f0
Milad-Soleymani <miladcoder1390@gmail.com>
2025-09-13 19:06:14 +0330

    Config events query params

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -8,19 +8,15 @@ import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
 import { eventsData, role } from '@/lib/data'
+import prisma from '@/lib/prisma'
+import { ITEM_PER_PAGE } from '@/lib/setting'
+import { Class, Event, Prisma } from '@prisma/client'
 import Image from 'next/image'
 import React from 'react'
 
 // 📌 Type definition for Event object
 // فارسی: تعریف نوع داده رویداد
-type Event = {
-  id: number
-  title: string
-  class: string
-  date: string
-  startTime: string
-  endTime: string
-}
+type EventList = Event & {class: Class}
 
 // 📌 Table Columns Configuration
 // فارسی: تنظیمات ستون‌های جدول
@@ -32,47 +28,98 @@ const columns = [
   { header: "زمان پایان", accessor: "endTime", className: "hidden md:table-cell" },
   { header: "اعمال", accessor: "actions" }
 ]
+// 📌 Render a single row of the table
+// فارسی: رندر کردن یک ردیف جدول
+const renderRow = (item: EventList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-specialPurpleLight"
+  >
+    {/* Event Title */}
+    {/* فارسی: عنوان رویداد */}
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
 
-const EventListPage = () => {
-  // 📌 Render a single row of the table
-  // فارسی: رندر کردن یک ردیف جدول
-  const renderRow = (item: Event) => (
-    <tr 
-      key={item.id} 
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-specialPurpleLight"
-    >
-      {/* Event Title */}
-      {/* فارسی: عنوان رویداد */}
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
+    {/* Class */}
+    <td>{item.class.name}</td>
 
-      {/* Class */}
-      <td>{item.class}</td>
+    {/* Date */}
+    <td className="hidden md:table-cell">{new Intl.DateTimeFormat("fa-IR").format(item.startTime)}</td>
 
-      {/* Date */}
-      <td className="hidden md:table-cell">{item.date}</td>
+    {/* Start Time */}
+    <td className="hidden md:table-cell">{item.startTime.toLocaleTimeString("fa-IR",{
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false
+    })}</td>
 
-      {/* Start Time */}
-      <td className="hidden md:table-cell">{item.startTime}</td>
+    {/* End Time */}
+    <td className="hidden md:table-cell">{item.endTime.toLocaleTimeString("fa-IR",{
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false
+    })}</td>
 
-      {/* End Time */}
-      <td className="hidden md:table-cell">{item.endTime}</td>
+    {/* Actions */}
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            {/* Update Event */}
+            <FormModal table="event" type="update" data={item} />
 
-      {/* Actions */}
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              {/* Update Event */}
-              <FormModal table="event" type="update" data={item} />
+            {/* Delete Event */}
+            <FormModal table="event" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+)
 
-              {/* Delete Event */}
-              <FormModal table="event" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
+const EventListPage = async ({ searchParams }: { searchParams: Promise<{ [key: string]: string | undefined }> }) => {
+
+  // console.log(data)
+
+  const params = await searchParams;
+  console.log(params);
+
+  const { page, ...queryParams } = params;
+  const p = page ? parseInt(page) : 1;
+
+  const query: Prisma.EventWhereInput = {};
+
+  // ! URL PARAMS CONDITIONS
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+
+        switch (key) {
+          case "search":
+            query.title = { contains: value, mode: "insensitive" }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+
+  const [data, count] = await prisma.$transaction([
+
+    prisma.event.findMany({
+      where: query,
+      include: {
+        class: true
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1)
+    }),
+    prisma.event.count({ where: query })
+  ]
   )
+
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -81,7 +128,7 @@ const EventListPage = () => {
       <div className="flex justify-between">
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <div className="flex items-center gap-4 self-end">
-            
+
             {/* Filter Button */}
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-specialYellow">
               <Image src="/filter.png" width={14} height={14} alt="Filter" />
@@ -106,11 +153,11 @@ const EventListPage = () => {
 
       {/* 📌 EVENTS TABLE */}
       {/* فارسی: جدول رویدادها */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
 
       {/* 📌 PAGINATION */}
       {/* فارسی: صفحه‌بندی */}
-      <Pagination />
+      <Pagination page={p} count={count}/>
     </div>
   )
 }
