b1fc048a92a45e7f2ef2a60310b8683b46a564d8
Milad-Soleymani <miladcoder1390@gmail.com>
2025-09-16 23:32:03 +0330

    Add get user role module

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import { auth } from "@clerk/nextjs/server";
+
+// تابع async برای گرفتن رول کاربر
+export const getUserRole = async () => {
+  const { sessionClaims } = await auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role || "";
+  return role;
+};
