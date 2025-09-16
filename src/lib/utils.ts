32aca1d1bd0a5a4369500c68704b84acb12b48d4
Milad-Soleymani <miladcoder1390@gmail.com>
2025-09-17 00:11:54 +0330

    Add userId to outpot

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 
 // تابع async برای گرفتن رول کاربر
 export const getUserRole = async () => {
-  const { sessionClaims } = await auth();
+  const { userId,sessionClaims } = await auth();
   const role = (sessionClaims?.metadata as { role?: string })?.role || "";
-  return role;
+  return {
+    role,
+    currentUserId: userId
+  }
 };
