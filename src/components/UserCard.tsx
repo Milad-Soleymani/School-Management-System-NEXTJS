a67d73351b0ee63cfd544a009e0dfa5683670dd7
Milad-Soleymani <miladcoder1390@gmail.com>
2025-09-19 21:29:35 +0330

    Use real data in page of admin (User Cards) (Not Temporary)

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,15 +1,36 @@
+
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 import React from "react";
-
+type UserType = "admin" | "teacher" | "student" | "parent";
 interface UserCardProps {
-  type: string; // نوع کاربر یا داده‌ای که باید نمایش داده شود
+  type: UserType; // نوع کاربر یا داده‌ای که باید نمایش داده شود
 }
 
-const UserCard: React.FC<UserCardProps> = ({ type }) => {
+const UserCard = async ({ type }: UserCardProps) => {
+
+  const modelMap: Record<UserType, any> = {
+    admin: prisma.admin,
+    teacher: prisma.teacher,
+    student: prisma.student,
+    parent: prisma.parent
+  }
+
+  const typeMap: Record<UserType, string> = {
+  admin: "مدیر",
+  teacher: "معلم",
+  student: "دانش‌آموز",
+  parent: "اولیا",
+};
+
+
+  const data = await modelMap[type].count();
+  const persianNumber = data.toLocaleString("fa-IR")
+  // console.log(data)
   return (
     // کارت اصلی با رنگ متناوب برای هر کارت
     <div className="rounded-2xl odd:bg-specialPurple even:bg-specialYellow p-4 flex-1 min-w-[130px]">
-      
+
       {/* هدر کارت شامل تاریخ و آیکون گزینه‌ها */}
       <div className="flex justify-between items-center">
         <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
@@ -19,10 +40,10 @@ const UserCard: React.FC<UserCardProps> = ({ type }) => {
       </div>
 
       {/* مقدار اصلی */}
-      <h1 className="text-2xl font-semibold my-3">۱،۲۳۴</h1>
+      <h1 className="text-2xl font-semibold my-3">{persianNumber}</h1>
 
       {/* نوع کارت */}
-      <h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
+      <h2 className="capitalize text-sm font-medium text-gray-500">{typeMap[type]}</h2>
     </div>
   );
 };
