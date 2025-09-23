c7172c992bc53bcc21b1e205da4ab1e535f543a6
Milad-Soleymani <miladcoder1390@gmail.com>
2025-09-23 20:51:41 +0330

    Debug

--- src/lib/formValidationSchema.ts
+++ src/lib/formValidationSchema.ts
@@ -1,8 +1,9 @@
-import z from "zod";
+import { z } from "zod";
 
 // تعریف اسکیمای اعتبارسنجی با Zod
 export const subjectSchema = z.object({
-  name: z.string()
-    .min(1, { message: 'نام کاربری الزامی است' })
-})
+  name: z.string().min(1, { message: 'نام کاربری الزامی است' }),
+});
+
+// تایپ ورودی استخراج شده از اسکیمای بالا
 export type SubjectSchema = z.infer<typeof subjectSchema>;
