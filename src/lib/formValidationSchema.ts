4ced66b02243b01f1041566c37ed530e944e83ea
Milad-Soleymani <miladcoder1390@gmail.com>
2026-06-02 10:26:45 +0330

    Add schema of class

--- src/lib/formValidationSchema.ts
+++ src/lib/formValidationSchema.ts
@@ -7,3 +7,14 @@ export const subjectSchema = z.object({
 });
 
 export type SubjectSchema = z.infer<typeof subjectSchema>;
+
+
+export const classSchema = z.object({
+  id: z.number().optional(),
+  name: z.string().min(1, { message: "نام کلاس الزامی است" }),
+  capacity: z.coerce.number().min(1, ("ظرفیت کلاس الزامی است")),
+  gradeId: z.coerce.number().min(1, ("نام پایه الزامی است")),
+  supervisorId: z.string().optional(),
+});
+
+export type ClassSchema = z.infer<typeof classSchema>;
