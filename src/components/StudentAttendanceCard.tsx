9a0d95a456720a210ab04271add061143ff15c7a
Milad-Soleymani <miladcoder1390@gmail.com>
2026-07-20 13:43:42 +0330

    Debug

--- src/components/StudentAttendanceCard.tsx
+++ src/components/StudentAttendanceCard.tsx
@@ -5,9 +5,7 @@ const StudentAttendanceCard = async ({ id }: { id: string }) => {
     const attendance = await prisma.attendance.findMany({
         where: {
             studentId: id,
-            // date: {
-            //     gte: new Date(new Date().getFullYear(), 0, 1)
-            // }
+         
         }
     })
 
